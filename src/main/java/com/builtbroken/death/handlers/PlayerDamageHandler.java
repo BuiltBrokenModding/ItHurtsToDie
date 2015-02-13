3cb6baded9c6ff21e3605491c92f8fdfd1aac63f
Robert S <rseifert.phone@gmail.com>
2015-02-13 01:17:13 -0500

    Some ideas for later

---
+++ src/main/java/com/builtbroken/death/handlers/PlayerDamageHandler.java
@@ -0,0 +1,14 @@
+package com.builtbroken.death.handlers;
+
+/**
+ * Created by robert on 2/13/2015.
+ */
+public class PlayerDamageHandler
+{
+    //TODO break items when hit by explosive damage
+    //TODO break items when hit by high fall damage
+    //TODO break items when hit with meele damage
+    //TODO items to break... glass, chests, potions
+    //TODO cause wood armor to burn up faster when hit by fire damage
+    //TODO cause metal armor to burn the player when on fire
+}
